aaea55e21c727e7bb7f16eea725b2664e0c7ac5f
Andreea Butca <cristina13andreea@gmail.com>
2025-08-26 16:42:57 +0300

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,6 +1,32 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   const product = products.find(p => p.id === productId);
   return product;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -662,4 +688,5 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => { return new Product(productDetails); });
+
